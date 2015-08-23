2a27b240ad5dd81c07a9b7f3769b12015d491691
GagnonStyle <spencer.gagnon95@gmail.com>
2015-08-23 16:36:54 -0400

    Added handling for shortened urls, also now we handle posting too often with grace

--- flickr_direct_link_bot.py
+++ flickr_direct_link_bot.py
@@ -5,6 +5,7 @@ from flickr_api.api import flickr
 import xml.etree.ElementTree as ET
 from urlparse import urlparse
 import praw
+import urllib
 import pdb
 import re
 import os
@@ -13,7 +14,7 @@ from config_bot import *
 # Setup flickr api
 flickr_api.set_keys(api_key = FLICKR_API_KEY, api_secret = FLICKR_API_SECRET)
 # Create the Reddit instance
-user_agent = ("flickr_to_imgur 0.1")
+user_agent = ("flickr_direct_link")
 r = praw.Reddit(user_agent=user_agent)
 r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
 subreddit = r.get_subreddit(SUBREDDIT_NAME)
@@ -34,7 +35,7 @@ for post_id in posts_replied_to:
     post = r.get_submission(submission_id=post_id)
     comments = post.comments
     for c in comments:
-        if c.author != None and c.author.name == 'FlickrToImgurBot' and c.body.startswith('###[Direct Photo Link]') and c.replies:
+        if c.author != None and c.author.name == REDDIT_USERNAME and c.body.startswith('###[Direct Photo Link]') and c.replies:
             for reply in c.replies:
                 if 'remove' in reply.body.lower() and reply.author.name == post.author.name:
                     print 'Found a link to remove, deleting...'
@@ -47,11 +48,18 @@ for submission in subreddit.get_new(limit=10):
     if submission.id not in posts_replied_to:
 
         link_url = urlparse(submission.url)
-        if link_url.netloc == 'www.flickr.com':
+        if link_url.netloc == 'www.flickr.com' or link_url.netloc =='flic.kr':
+
             print 'Found a new Flickr post!'
+            #deal with shortened urls
+            if link_url.netloc == 'flic.kr':
+                sub_url = urllib.urlopen(submission.url).url
+            else:
+                sub_url = submission.url 
+
             #regex out the photo id, so we can use it in the flickr API
-            if re.search('photos/[^/]+/([0-9]+)', submission.url) != None:
-                photo_id = re.search('photos/[^/]+/([0-9]+)', submission.url).group(1)
+            if re.search('photos/[^/]+/([0-9]+)', sub_url) != None:
+                photo_id = re.search('photos/[^/]+/([0-9]+)', sub_url).group(1)
 
                 # Get all the sizes of the flickr photo
                 print 'Getting image from Flickr...'
@@ -65,15 +73,18 @@ for submission in subreddit.get_new(limit=10):
                     image_url = biggest_size.get('source')
 
                     print 'Posting Comment...'
+                    try:
+                        submission.add_comment('###[Direct Photo Link](' + image_url + ')\n' +
+                                               '*****\n' +
+                                               '^^Are you OP? Would you like this link removed?\n' +
+                                               '^^Just comment \'remove\' and the bot will trash it the next time it runs!\n' +
+                                               '^^This bot uses the flickr API to get a direct link to the posted photo.\n' +
+                                               '^^It does not rehost or mirror the image in any way. Check out the source code on [GitHub](https://github.com/GagnonStyle/flickr_to_imgur)')
+                        posts_replied_to.append(submission.id)
+                        print 'Comment Posted!'
+                    except praw.errors.RateLimitExceeded, e:
+                        print e
 
-                    submission.add_comment('###[Direct Photo Link](' + image_url + ')\n' +
-                                           '*****\n' +
-                                           '^^Are you OP? Would you like this link removed?\n' +
-                                           '^^Just comment \'remove\' and the bot will trash it the next time it runs!\n' +
-                                           '^^This bot uses the flickr API to get a direct link to the posted photo.\n' +
-                                           '^^It does not rehost or mirror the image in any way. Check out the source code on [GitHub](https://github.com/GagnonStyle/flickr_to_imgur)')
-                    posts_replied_to.append(submission.id)
-                    print 'Comment Posted!'
                 else:
                     print 'Can\'t rehost this photo! (copyright stuff...)'
             else:
