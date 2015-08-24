b3df5d4bb504fca8b41e95ef3a0d936918cde671
GagnonStyle <spencer.gagnon95@gmail.com>
2015-08-24 12:46:49 -0400

    Added support for multiple subreddits, and checks for broken links.
    Also some fancier formatting and prints!

---
+++ bot.py
@@ -0,0 +1,84 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+import praw
+import re
+from urlparse import urlparse
+import xml.etree.ElementTree as ET
+from flickr_api.api import flickr
+
+def new_comment(submission):
+    parsed_url = urlparse(submission.url)
+    real_url = get_real_link_url(parsed_url)
+    #regex out the photo id, so we can use it in the flickr API
+    if re.search('photos/[^/]+/([0-9]+)', real_url) != None:
+        photo_id = re.search('photos/[^/]+/([0-9]+)', real_url).group(1)
+
+        # Get all the sizes of the flickr photo
+        print 'Getting image from Flickr...'
+        info = flickr.photos.getInfo(photo_id = photo_id)
+        if ET.fromstring(info).findall('photo/usage')[0].get('candownload') == '1':
+            sizes_xml = flickr.photos.getSizes(photo_id = photo_id)
+            # Last == Biggest
+            sizes = ET.fromstring(sizes_xml).findall('sizes/size')
+            biggest_size = sizes[-1]
+
+            image_url = biggest_size.get('source')
+
+            print 'Posting Comment...'
+            try:
+                submission.add_comment('###[Direct Photo Link](' + image_url + ')\n' +
+                                       '*****\n' +
+                                       '^^Are you OP? Would you like this link removed?\n' +
+                                       '^^Just comment \'remove\' and the bot will trash it the next time it runs!\n' +
+                                       '^^This bot uses the flickr API to get a direct link to the posted photo.\n' +
+                                       '^^It does not rehost or mirror the image in any way. Check out the source code on [GitHub](https://github.com/GagnonStyle/flickr_to_imgur)')
+                print 'Comment Posted!'
+                return True
+            except praw.errors.RateLimitExceeded, e:
+                print e
+        else:
+            print 'Can\'t rehost this photo! (copyright stuff...)'
+    else:
+        print 'The post was an album, too bad!'
+    return False
+def edit_comment(submission, comment):
+    parsed_url = urlparse(submission.url)
+    real_url = get_real_link_url(parsed_url)
+    #regex out the photo id, so we can use it in the flickr API
+    if re.search('photos/[^/]+/([0-9]+)', real_url) != None:
+        photo_id = re.search('photos/[^/]+/([0-9]+)', real_url).group(1)
+
+        # Get all the sizes of the flickr photo
+        print 'Getting image from Flickr...'
+        info = flickr.photos.getInfo(photo_id = photo_id)
+        if ET.fromstring(info).findall('photo/usage')[0].get('candownload') == '1':
+            sizes_xml = flickr.photos.getSizes(photo_id = photo_id)
+            # Last == Biggest
+            sizes = ET.fromstring(sizes_xml).findall('sizes/size')
+            biggest_size = sizes[-1]
+
+            image_url = biggest_size.get('source')
+
+            print 'Editing Comment...'
+            try:
+                comment.edit('###[Direct Photo Link](' + image_url + ')\n' +
+                             '*****\n' +
+                             '^^Are you OP? Would you like this link removed?\n' +
+                             '^^Just comment \'remove\' and the bot will trash it the next time it runs!\n' +
+                             '^^This bot uses the flickr API to get a direct link to the posted photo.\n' +
+                             '^^It does not rehost or mirror the image in any way. Check out the source code on [GitHub](https://github.com/GagnonStyle/flickr_to_imgur)')
+                print 'Comment Edited!'
+                return True
+            except praw.errors.RateLimitExceeded, e:
+                print e
+        else:
+            print 'Can\'t rehost this photo! (copyright stuff...)'
+    else:
+        print 'The post was an album, too bad!'
+    return False
+def get_real_link_url(parsed_url):
+    #deal with shortened urls
+    if parsed_url.netloc == 'flic.kr':
+        return urllib.urlopen(parsed_url.geturl()).url
+    else:
+        return parsed_url.geturl()
--- flickr_direct_link_bot.py
+++ flickr_direct_link_bot.py
@@ -2,22 +2,24 @@
 # -*- coding: utf-8 -*-
 import flickr_api
 from flickr_api.api import flickr
-import xml.etree.ElementTree as ET
 from urlparse import urlparse
 import praw
+import requests
 import urllib
 import pdb
-import re
 import os
+import time
 from config_bot import *
+from bot import *
 
+print '======' + time.strftime("%H:%M:%S") + '======'
 # Setup flickr api
 flickr_api.set_keys(api_key = FLICKR_API_KEY, api_secret = FLICKR_API_SECRET)
 # Create the Reddit instance
 user_agent = ("flickr_direct_link")
-r = praw.Reddit(user_agent=user_agent)
-r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
-subreddit = r.get_subreddit(SUBREDDIT_NAME)
+reddit = praw.Reddit(user_agent=user_agent)
+reddit.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
+subreddits = SUBREDDIT_NAMES
 
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile("posts_replied_to.txt"):
@@ -31,8 +33,10 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
+print 'Looking for comments to remove or fix.'   
 for post_id in posts_replied_to:
-    post = r.get_submission(submission_id=post_id)
+    post = reddit.get_submission(submission_id=post_id)
+
     comments = post.comments
     for c in comments:
         if c.author != None and c.author.name == REDDIT_USERNAME and c.body.startswith('###[Direct Photo Link]') and c.replies:
@@ -41,57 +45,37 @@ for post_id in posts_replied_to:
                     print 'Found a link to remove, deleting...'
                     c.delete()
                     break
-
-
-for submission in subreddit.get_new(limit=10):
-    # If we haven't replied to this post before
-    if submission.id not in posts_replied_to:
-
-        link_url = urlparse(submission.url)
-        if link_url.netloc == 'www.flickr.com' or link_url.netloc =='flic.kr':
-
-            print 'Found a new Flickr post!'
-            #deal with shortened urls
-            if link_url.netloc == 'flic.kr':
-                sub_url = urllib.urlopen(submission.url).url
-            else:
-                sub_url = submission.url 
-
-            #regex out the photo id, so we can use it in the flickr API
-            if re.search('photos/[^/]+/([0-9]+)', sub_url) != None:
-                photo_id = re.search('photos/[^/]+/([0-9]+)', sub_url).group(1)
-
-                # Get all the sizes of the flickr photo
-                print 'Getting image from Flickr...'
-                info = flickr.photos.getInfo(photo_id = photo_id)
-                if ET.fromstring(info).findall('photo/usage')[0].get('candownload') == '1':
-                    sizes_xml = flickr.photos.getSizes(photo_id = photo_id)
-                    # Last == Biggest
-                    sizes = ET.fromstring(sizes_xml).findall('sizes/size')
-                    biggest_size = sizes[-1]
-
-                    image_url = biggest_size.get('source')
-
-                    print 'Posting Comment...'
-                    try:
-                        submission.add_comment('###[Direct Photo Link](' + image_url + ')\n' +
-                                               '*****\n' +
-                                               '^^Are you OP? Would you like this link removed?\n' +
-                                               '^^Just comment \'remove\' and the bot will trash it the next time it runs!\n' +
-                                               '^^This bot uses the flickr API to get a direct link to the posted photo.\n' +
-                                               '^^It does not rehost or mirror the image in any way. Check out the source code on [GitHub](https://github.com/GagnonStyle/flickr_to_imgur)')
-                        posts_replied_to.append(submission.id)
-                        print 'Comment Posted!'
-                    except praw.errors.RateLimitExceeded, e:
-                        print e
-
-                else:
-                    print 'Can\'t rehost this photo! (copyright stuff...)'
-            else:
-                print 'The post was an album, too bad!'
-
+            break
+        #check if previous link still works
+        regex = re.match('###\[Direct Photo Link\]\((.*)\)', c.body)
+        if regex != None:
+            prev_link = regex.group(1)
+            response = requests.head(prev_link)
+            if response.status_code != 200:
+                print 'Broken link, editing to fix...'
+                #if the link is broken, edit it with a good link!
+                edit_comment(submission=post, comment=c)
+print 'Done looking.'
 print ''
+for sub_name in subreddits:
+    print 'Looking for new flickr posts in /r/' + sub_name
+    sub = reddit.get_subreddit(sub_name)
+    for submission in sub.get_new(limit=7):
+        # If we haven't replied to this post before
+        if submission.id not in posts_replied_to:
+
+            parsed_url = urlparse(submission.url)
+            if parsed_url.netloc == 'www.flickr.com' or parsed_url.netloc =='flic.kr':
+
+                print 'Found a new Flickr post!'
+
+                result = new_comment(submission=submission)
+                if result:
+                    posts_replied_to.append(submission.id)
+
+print 'Done!'
 # Write our updated list back to the file
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
+print '==========='
